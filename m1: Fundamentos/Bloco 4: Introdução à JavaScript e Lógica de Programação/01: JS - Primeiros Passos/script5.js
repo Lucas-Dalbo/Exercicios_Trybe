e8c2021cc52751c2530dde2ea6218fec8e4f6d17
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-02-02 19:20:12 -0300

    conclui exercicio 5

---
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/01: JS - Primeiros Passos/script5.js
@@ -0,0 +1,13 @@
+const a;
+const b;
+const c;
+
+if ( a < 0 || b < 0 || c < 0 ) {
+    console.log("Erro. Insira valores válidos")
+}
+else if ( a + b + c == 180 ) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
