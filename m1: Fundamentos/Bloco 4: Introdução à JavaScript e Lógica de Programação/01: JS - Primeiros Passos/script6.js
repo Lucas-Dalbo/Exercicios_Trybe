7868f9bb98fefc02e08385578da34af676859cc6
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-02-03 07:09:12 -0300

    resolve exercicio 6

---
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/01: JS - Primeiros Passos/script6.js
@@ -0,0 +1,31 @@
+let chessPice = "";
+let result = chessPice.toLocaleLowerCase();
+
+switch (result) {
+    case  "peão":
+        console.log("Primeiro movimento: duas casas a frente; Segundo movimento em diante: uma casa a frente");
+        break;
+    
+    case "torre":
+        console.log("Em lLinha reta na hrizontal ou na vertical");
+        break;
+    
+    case "cavalo":
+        console.log("Em L, na horizontal ou na vertical, 2 casas de altura e 1 de largura");
+        break;
+
+    case "bispo":
+        console.log("Em linha reta, na diagonal");
+        break;
+
+    case "rainha":
+        console.log("Em Linha reta, em qualquer direção");
+        break;
+        
+    case "rei":
+        console.log("Uma casa, em qualquer direção")
+        break;
+
+    default:
+        console.log("Erro. Peça invalida")
+}
