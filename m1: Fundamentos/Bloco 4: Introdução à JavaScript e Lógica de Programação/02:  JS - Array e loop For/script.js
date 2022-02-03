b698bd527c35e24a58025e7cd68756159fb3b4f3
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-02-03 18:46:01 -0300

    Resolve exercicio 5

--- m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/02:  JS - Array e loop For/script.js
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/02:  JS - Array e loop For/script.js
@@ -14,17 +14,28 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(soma);
 
 //Exercício 3 e 4
-let soma = 0;
+// let soma = 0;
 
-for ( let i = 0; i < numbers.length; i+=1 ) {
-    soma = soma + numbers[i];
-}
-let media = soma / 2;
-console.log(media);
+// for ( let i = 0; i < numbers.length; i+=1 ) {
+//     soma = soma + numbers[i];
+// }
+// let media = soma / 2;
+// console.log(media);
 
-if ( media > 20) {
-    console.log("Valor maior que 20");
+// if ( media > 20) {
+//     console.log("Valor maior que 20");
+// }
+// else {
+//     console.log("Valor menor que 20")
+// }
+
+// Exercício 5
+let maior = 0;
+
+for ( let element of numbers) {
+    if (maior < element) {
+        maior = element ;
+    }
 }
-else {
-    console.log("Valor menor que 20")
-}
+
+console.log(maior);
