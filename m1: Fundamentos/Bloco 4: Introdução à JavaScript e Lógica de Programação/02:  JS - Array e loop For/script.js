d2a863da6cf7b5d81d7fc9ce494b4d75e52d87c5
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-02-03 18:21:21 -0300

    Resolve exercicios 1, 2 e 3

--- m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/02:  JS - Array e loop For/script.js
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/02:  JS - Array e loop For/script.js
@@ -1,1 +1,22 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ 
+// Exercício 1
+// for ( let element of numbers ) {
+//     console.log(element);
+// }
+
+// Exercício 2
+// let soma = 0;
+
+// for ( let i = 0; i < numbers.length; i+=1 ) {
+//     soma = soma + numbers[i];
+// }
+// console.log(soma);
+
+//Exercício 3
+let soma = 0;
+
+for ( let i = 0; i < numbers.length; i+=1 ) {
+    soma = soma + numbers[i];
+}
+console.log(soma / 2);
