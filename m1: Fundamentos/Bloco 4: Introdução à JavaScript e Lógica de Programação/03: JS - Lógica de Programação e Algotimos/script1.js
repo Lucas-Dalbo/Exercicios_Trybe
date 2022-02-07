b6639781d9febe0bcccf1f548681abc4b09766e6
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-02-07 16:51:39 -0300

    corrige  exercicio 1

--- m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/03: JS - Lógica de Programação e Algotimos/script1.js
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/03: JS - Lógica de Programação e Algotimos/script1.js
@@ -1,9 +1,9 @@
 // EXERCÍCIO 1
 let number = 10;
-let fatorial = 0;
+let fatorial = 1;
 
-for ( let i = 0; i <= number; i+=1) {
-    fatorial = fatorial + i ;
+for ( let i = 1; i <= number; i+=1) {
+    fatorial = fatorial * i ;
 }
 
 console.log(fatorial);
