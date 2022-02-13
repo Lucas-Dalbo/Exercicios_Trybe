d863d0072211b7859404e298ee703f9c16dc93b9
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-02-13 17:21:45 -0300

    Resolve Bônus 4

--- m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/03: JS - Lógica de Programação e Algotimos/bonus.js
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/03: JS - Lógica de Programação e Algotimos/bonus.js
@@ -2,92 +2,126 @@ const caractere = "*";
 const espaco = " ";
 
 // 1. Retângulo
-function retangulo (size) {
+function retangulo(size) {
     let linha = "";
-    for ( let i = 1; i <= size; i += 1) {
+    for (let i = 1; i <= size; i += 1) {
         linha += caractere;
     }
-    for ( let i = 1; i <= size; i += 1) {
+    for (let i = 1; i <= size; i += 1) {
         console.log(linha);
     }
 };
 
 // 2. Triângulo Retângulo
-function triRet (size) {
+function triRet(size) {
     let linha = "";
-    for ( let i = 1; i <= size; i += 1) {
+    for (let i = 1; i <= size; i += 1) {
         linha += caractere;
         console.log(linha);
     }
 };
 
 // 3. Triângulo Retângulo do outro lado
-function triRetRev (size) {
-    for ( let i = 1; i <= size; i += 1) {
+function triRetRev(size) {
+    for (let i = 1; i <= size; i += 1) {
         let linha = "";
-        if ( i === 1) {
-            for ( let j = 1; j<= size; j+=1 ) {
-                if ( j === 5) {
+        if (i === 1) {
+            for (let j = 1; j <= size; j += 1) {
+                if (j === 5) {
                     linha += caractere;
                 }
                 else {
                     linha += espaco;
                 }
             }
-            console.log(linha); 
+            console.log(linha);
         }
-                
-         linha = ""
-        if ( i === 2) {
-            for ( let j = 1; j<= size; j+=1 ) {
-                if ( j >= 4) {
-                    linha += caractere;
-                }
-                else {
-                    linha += espaco;
-                }
-            }
-        console.log(linha);     
-        }
-        
 
-         linha = ""
-        if ( i === 3) {
-            for ( let j = 1; j<= size; j+=1 ) {
-                if ( j >= 3) {
+        linha = ""
+        if (i === 2) {
+            for (let j = 1; j <= size; j += 1) {
+                if (j >= 4) {
                     linha += caractere;
                 }
                 else {
                     linha += espaco;
                 }
             }
-        console.log(linha);     
+            console.log(linha);
         }
-        
 
-         linha = ""
-        if ( i === 4) {
-            for ( let j = 1; j<= size; j+=1 ) {
-                if ( j >= 2 ) {
+        linha = ""
+        if (i === 3) {
+            for (let j = 1; j <= size; j += 1) {
+                if (j >= 3) {
                     linha += caractere;
                 }
                 else {
                     linha += espaco;
                 }
             }
-        console.log(linha);     
+            console.log(linha);
         }
-        
 
-         linha = ""
-        if ( i === 5) {
-            for ( let j = 1; j<= size; j+=1 ) {
+        linha = ""
+        if (i === 4) {
+            for (let j = 1; j <= size; j += 1) {
+                if (j >= 2) {
+                    linha += caractere;
+                }
+                else {
+                    linha += espaco;
+                }
+            }
+            console.log(linha);
+        }
+
+        linha = ""
+        if (i === 5) {
+            for (let j = 1; j <= size; j += 1) {
                 linha += caractere;
             }
-        console.log(linha);     
+            console.log(linha);
         }
-        
+
     }
 };
 
-triRetRev(5)
+// 4. Piramide 
+function piramide(size) {
+    for (let i = 1; i <= size - 2; i += 1) {
+        let linha = "";
+        if (i === 1) {
+            for (let j = 1; j <= size; j += 1) {
+                if (j === 3) {
+                    linha += caractere;
+                }
+                else {
+                    linha += espaco;
+                }
+            }
+            console.log(linha);
+        }
+
+        linha = "";
+        if (i === 2) {
+            for (let j = 1; j <= size; j += 1) {
+                if (j > 1 && j < 5) {
+                    linha += caractere;
+                }
+                else {
+                    linha += espaco;
+                }
+            }
+            console.log(linha);
+        }
+
+        linha = "";
+        if (i === 3) {
+            for (let j = 1; j <= size; j += 1) {
+                linha += caractere;
+            }
+            console.log(linha);
+        }
+    }
+};
