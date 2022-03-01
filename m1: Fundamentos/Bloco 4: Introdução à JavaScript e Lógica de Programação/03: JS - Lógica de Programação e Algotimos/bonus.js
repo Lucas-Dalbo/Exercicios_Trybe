144378b1e42777817463524fb8b89b79cc8cd978
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-01 09:47:25 -0300

    Resolve Atividade 3

--- m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/03: JS - Lógica de Programação e Algotimos/bonus.js
+++ m1: Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/03: JS - Lógica de Programação e Algotimos/bonus.js
@@ -21,12 +21,31 @@ function triRet(size) {
     }
 };
 
-// 6. Número Ptimo
+// 3. Triângulo oposto
+function triOps(size) {
+  let posicionador = size;
+  for (let i = 1; i <= size; i += 1) {
+    let linha = '';
+    for (let j = 1; j <= size; j += 1) {
+      if (j >= posicionador) {
+        linha += caractere;
+      }
+      else {
+        linha += espaco;
+      }
+    }
+    console.log(linha);
+    posicionador -= 1;
+  }
+}
+triOps(5);
+
+// 6. Número Primo
 let number = 49;
 let ePrimo = true;
 
-for ( let divisor = 2; divisor < number ; divisor += 1) {
-    if ( number % divisor === 0) {
+for (let divisor = 2; divisor < number; divisor += 1) {
+    if (number % divisor === 0) {
         ePrimo = false;
         break;
     }
