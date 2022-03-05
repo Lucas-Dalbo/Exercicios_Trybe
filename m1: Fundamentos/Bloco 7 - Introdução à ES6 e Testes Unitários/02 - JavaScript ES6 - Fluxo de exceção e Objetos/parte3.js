1ec40f16157c54029cde3574735310ce57938e98
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-05 13:26:49 -0300

    Resolve ate a atividade 5

--- m1: Fundamentos/Bloco 7 - Introdução à ES6 e Testes Unitários/02 - JavaScript ES6 - Fluxo de exceção e Objetos/parte3.js
+++ m1: Fundamentos/Bloco 7 - Introdução à ES6 e Testes Unitários/02 - JavaScript ES6 - Fluxo de exceção e Objetos/parte3.js
@@ -23,3 +23,25 @@ const lesson1 = {
   
   newKey(lesson2, 'turno', 'noite');
   console.log(lesson2);
+
+  // 2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+  const objKeys = obj => Object.keys(obj);
+
+  console.log(objKeys(lesson1));
+
+  // 3. Crie uma função para mostrar o tamanho de um objeto.
+  const objLength = obj => Object.keys(obj).length;
+
+  console.log(objLength(lesson1));
+
+  // 4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro
+  const objValues = obj => Object.values(obj);
+
+  console.log(objValues(lesson3));
+
+  // 5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+  const allLessons = (obj1, obj2, obj3) => {
+    let allLessons = {lesson1: Object.assign({}, obj1), lesson2: Object.assign({},obj2), lesson3: Object.assign({},obj3)};
+    console.log(allLessons);
+  }
+  allLessons(lesson1,lesson2,lesson3);
