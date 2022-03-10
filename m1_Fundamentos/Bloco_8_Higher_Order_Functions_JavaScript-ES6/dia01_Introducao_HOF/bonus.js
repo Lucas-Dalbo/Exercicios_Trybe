cd6f63d8159ea7899f49a972d0cea49e85475f62
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-10 07:31:01 -0300

    Ajusta dano do Mage

--- m1_Fundamentos/Bloco_8_Higher_Order_Functions_JavaScript-ES6/dia01_Introducao_HOF/bonus.js
+++ m1_Fundamentos/Bloco_8_Higher_Order_Functions_JavaScript-ES6/dia01_Introducao_HOF/bonus.js
@@ -32,17 +32,28 @@ const warriorDMG = () => Math.floor(Math.random() * (warrior.weaponDmg * warrior
 // 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
 // A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
-const mageDMG = () => ({
-  dmg: Math.floor(Math.random() * (mage.intelligence * 2 - mage.intelligence)) + mage.intelligence,
-  mana: manaCost,
+const mageDMG = (dano, custo) => ({
+  dmg: dano(),
+  manaCost: custo(),
 });
+
 const manaCost = () => {
-    if( mage.mana >= 15) {
-        mage['mana'] -= 15
-        return '-15 mana'
-    } else {
-        return 'Sem mana suficiente'
-    }
+  if( mage.mana >= 15) {
+      return 15
+  } else {
+      return 0
+  };
 }
 
-console.log(dragonDMG(), warriorDMG(), mageDMG().dmg, mageDMG().mana(), mage.mana);
+const spellDmg = () => {
+  let damage;
+  if( mage.mana >= 15) {
+    mage['mana'] -= 15;
+    damage = Math.floor(Math.random() * (mage.intelligence * 2 - mage.intelligence)) + mage.intelligence;
+  } else {
+    damage = 'Sem mana suficiente'
+  };
+  return damage
+}
+
+console.log(dragonDMG(), warriorDMG(), mageDMG(spellDmg, manaCost), mage.mana);
