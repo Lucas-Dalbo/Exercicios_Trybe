89fbd6a3fa4b6a9ab46a96ae3dac8ceeb0a9add1
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-28 15:05:42 -0300

    Resolve bonus 3

---
+++ m1_Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/04: JS - Obejtos e Funções/bonus3.js
@@ -0,0 +1,38 @@
+// 3 - A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor.
+// Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+function fruitCount(array) {
+  const fruit = {};
+  for(element of array) {
+    if(fruit[element] === undefined) {
+      fruit[element] = 1;
+    } else {
+      fruit[element] = fruit[element] + 1;
+    }
+  }
+  return fruit;
+}
+
+const basketObject = fruitCount(basket);
+const { Melancia, Abacate, Uva, Laranja, Jaca, Pera, Banana } = basketObject;
+console.log(`Sua cesta possui: ${Melancia} melancias, ${Abacate} abacates, ${Uva} uvas, ${Laranja} laranajs, ${Jaca} jacas, ${Pera} peras e ${Banana} bananas`);
