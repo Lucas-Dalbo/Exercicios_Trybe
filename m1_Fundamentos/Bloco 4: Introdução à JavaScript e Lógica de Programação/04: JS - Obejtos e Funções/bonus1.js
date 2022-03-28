e07b6c7c9115d1ac7efd40edede641312b26f394
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-28 14:48:08 -0300

    Resolve bonus 2 e add anunciado Bonus 1

--- m1_Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/04: JS - Obejtos e Funções/bonus1.js
+++ m1_Fundamentos/Bloco 4: Introdução à JavaScript e Lógica de Programação/04: JS - Obejtos e Funções/bonus1.js
@@ -8,6 +8,8 @@ const romamNumbers = {
   M: 1000,
 }
 
+// 1- Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
 function indoArabicNumber (string) {
   try {
     const algarisms = string.split('');
