457229fd6da0d80fefa23c3a64209250558e45e2
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-29 18:25:55 -0300

    Resolve ex6 bloco 7.5

---
+++ m1_Fundamentos/Bloco_7_Introducao_ a_ES6_e_Testes_Unitarios/Dia_03 _Jest _ Primeiros_passos/exercicios/hydrate.js
@@ -0,0 +1,10 @@
+const hydrate = (bebidas) => {
+  const resultado = bebidas.replace(/[^\d]/g, '')
+    .split('')
+    .reduce((acc, crr) => acc + Number(crr), 0);
+
+  if (resultado === 1) return '1 copo de água';
+  return `${resultado} copos de água`;
+}
+
+module.exports = hydrate;
