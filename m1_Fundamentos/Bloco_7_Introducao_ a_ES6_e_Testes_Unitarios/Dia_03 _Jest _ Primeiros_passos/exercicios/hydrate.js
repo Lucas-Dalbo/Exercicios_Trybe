633aaaf87eeb11a15a67ec9a74f4e84340babee8
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-29 18:54:27 -0300

    Add alternativa Exercicio 6 bloco 7.3

--- m1_Fundamentos/Bloco_7_Introducao_ a_ES6_e_Testes_Unitarios/Dia_03 _Jest _ Primeiros_passos/exercicios/hydrate.js
+++ m1_Fundamentos/Bloco_7_Introducao_ a_ES6_e_Testes_Unitarios/Dia_03 _Jest _ Primeiros_passos/exercicios/hydrate.js
@@ -1,9 +1,26 @@
-const hydrate = (bebidas) => {
-  const resultado = bebidas.replace(/[^\d]/g, '')
-    .split('')
-    .reduce((acc, crr) => acc + Number(crr), 0);
-  if (resultado === 1) return '1 copo de água';
-  return `${resultado} copos de água`;
+// Só funciona quando o número de bebidas é menor que 10.
+// const hydrate = (bebidas) => {
+//   const resultado = bebidas.replace(/[^\d]/g, '')
+//     .split('')
+//     .reduce((acc, crr) => acc + Number(crr), 0);
+//   if (resultado === 1) return '1 copo de água';
+//   return `${resultado} copos de água`;
+// }
+
+
+// Funciona para qualquer quantidade.
+// documentação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+function hydrate(string) {
+  const id = (/\d+/g);
+  let num;
+  let sum = 0;
+  while ((num = id.exec(string)) !== null) {
+    sum += Number(num);
+  }
+  if ( sum === 1 ) return "1 copo de água";
+  return `${sum} copos de água`;
 }
+
+console.log(hydrate('10 cervejas, 2 tequilas'));
 
 module.exports = hydrate;
