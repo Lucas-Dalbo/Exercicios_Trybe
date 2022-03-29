52a9849fc8bcd44f8f7a321a3778206440a665d3
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-03-29 14:52:18 -0300

    Resolve exercicio 2 bloco 7.3

--- m1_Fundamentos/Bloco_7_Introducao_ a_ES6_e_Testes_Unitarios/Dia_03 _Jest _ Primeiros_passos/exercicios/remove.test.js
+++ m1_Fundamentos/Bloco_7_Introducao_ a_ES6_e_Testes_Unitarios/Dia_03 _Jest _ Primeiros_passos/exercicios/remove.test.js
@@ -11,7 +11,15 @@ function myRemove(arr, item) {
   // implemente seus testes aqui
 
   describe('Função myRemove', () => {
-    it('Se a entrada for [1,2,3,4] e 3, deve retornar [1,2,3]', () => {
-        expect(myRemove([1, 2, 3, 4], 3)).not.toContain(3);
-    }); 
+    const testArray = [1, 2, 3, 4];
+    it('Se a entrada for [1,2,3,4] e 3, deve retornar [1,2,4]', () => {
+        expect(myRemove(testArray, 3)).toEqual([1, 2, 4]);
+    });
+    it('Se a função não retorna o primeiro argumento', () => {
+      const expected = myRemove(testArray, 3);
+      expect(expected).not.toEqual(testArray);
+    });
+    it('Se a entrada for (testArray, 5) deve retornar testArray', () => {
+      expect(myRemove(testArray, 5)).toEqual(testArray);
+    });
   });
