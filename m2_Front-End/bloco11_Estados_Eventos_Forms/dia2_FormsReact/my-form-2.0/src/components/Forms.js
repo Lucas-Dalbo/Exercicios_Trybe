9520ab3e880cc9b1023ccd1e76ede71db4516921
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-04-10 12:35:19 -0300

    Adiciona as funcionalidades pedidas aos inputs

--- m2_Front-End/bloco11_Estados_Eventos_Forms/dia2_FormsReact/my-form-2.0/src/components/Forms.js
+++ m2_Front-End/bloco11_Estados_Eventos_Forms/dia2_FormsReact/my-form-2.0/src/components/Forms.js
@@ -4,20 +4,63 @@ import estados from "../data";
 import './Forms.css'
 
 class Forms extends React.Component {
+  constructor() {
+    super()
+    this.handleInfo = this.handleInfo.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+
+    this.state = {
+      nome: '',
+      email: '',
+      cpf: '',
+      endereço: '',
+      cidade: '',
+      estado: '',
+      residencia: '',
+      resumo: '',
+      cargo: '',
+      resumoCargo: ''
+    }
+  }
+
+  onMouseOver(e) {
+    if(e.target.classList.length === 0) {
+      alert('Preencha com cuidado esta informação.')
+    }
+    e.target.className = "show"
+  }
+
+  onBlur({target}) {
+    let { value } = target;
+    const verificador = value.replace(/[^0-9]/g, '');
+    if(verificador.length > 0) value = '';
+    this.setState({
+      cidade: value,
+    })
+  }
+
+  handleInfo({ target }) {
+    let {name, value} = target;
+    if(name === "nome") value = value.toUpperCase();
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
     return(
       <form className="form">
         <legend>Dados Pessoais</legend>
         <fieldset className="container">
-          <Input name="nome" length="40" text="Nome" />
-          <Input name="email" length="50" text="Email" />
-          <Input name="cpf" length="11" text="CPF" />
-          <Input name="endereço" length="200" text="Endereço" />
+          <Input name="nome" length="40" text="Nome" value={this.state.nome} onChange={this.handleInfo} />
+          <Input name="email" length="50" text="Email" onChange={this.handleInfo} />
+          <Input name="cpf" length="11" text="CPF" onChange={this.handleInfo} />
+          <Input name="endereço" length="200" text="Endereço" onChange={this.handleInfo} />
 
           <div className="cidadeEstado">
-          <Input name="cidade" length="28" text="Cidade" />
+          <Input name="cidade" length="28" text="Cidade" value={this.state.cidade} onChange={this.handleInfo} onBlur={this.onBlur} />
           <label> {'Estado'}
-            <select defaultValue="none" name="estado" required>
+            <select defaultValue="none" name="estado" onChange={this.handleInfo} required>
               <option value="none"></option>
               {estados.map((map)=> <option key={map} value={map}>{map}</option>)}
             </select>
@@ -27,11 +70,11 @@ class Forms extends React.Component {
           <div className="radio">
             <spam>{'Residência: '}</spam>
             <label> 
-              <input type={'radio'} name="residencia"></input>
+              <input type={'radio'} name="residencia" value={'Casa'} onChange={this.handleInfo} required></input>
               Casa
             </label>
             <label> 
-              <input type={'radio'} name="residencia"></input>
+              <input type={'radio'} name="residencia" value={'Apartamento'} onChange={this.handleInfo} required></input>
               Apartamento
             </label>
           </div>
@@ -41,15 +84,15 @@ class Forms extends React.Component {
         <fieldset className="container">
           <label className="text-container">
             Resumo do Curriculo
-            <textarea name="resumo" maxLength={1000} required></textarea>
+            <textarea name="resumo" maxLength={1000} required onChange={this.handleInfo}></textarea>
           </label>
           <label className="text-container">
             Cargo
-            <textarea name="resumo" maxLength={40} required></textarea>
+            <textarea name="cargo" maxLength={40} required onMouseOver={this.onMouseOver} onChange={this.handleInfo}></textarea>
           </label>
           <label className="text-container">
             Descrição do Cargo
-            <textarea name="cargo" maxLength={500} required></textarea>
+            <textarea name="resumoCargo" maxLength={500} required onChange={this.handleInfo}></textarea>
           </label>
         </fieldset>
       </form>
--- m2_Front-End/bloco11_Estados_Eventos_Forms/dia2_FormsReact/my-form-2.0/src/components/Input.js
+++ m2_Front-End/bloco11_Estados_Eventos_Forms/dia2_FormsReact/my-form-2.0/src/components/Input.js
@@ -3,14 +3,17 @@ import "./Input.css";
 
 class Input extends React.Component {
   render() {
-    const { name, length, text } = this.props;
+    const { name, length, text, onChange, value, onBlur } = this.props;
 
     return(
       <label className="input"> {text}
         <input
           type={'text'}
-          name={name}
-          maxLength={length}
+          name={ name }
+          maxLength={ length }
+          onChange={ onChange }
+          onBlur={ onBlur }
+          value={ value }
           required
           >
         </input> 
