8cdbf6e4bd25b333347fa619aa61c06c1bede58b
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-06 18:49:30 -0300

    Exercício 19.2 Trabalhando com Docker e Dockerfiles.

---
+++ m3: Back-End/bloco19_docker_utilizando_containers/dia02_manipulando_imagens_docker/atividade_19_2.sh
@@ -0,0 +1,7 @@
+
+ 2039  docker build -t cowsay .
+ 2040  docker container run cowsay
+ 2041  docker container run cowsay que coisa, não?
+ 2042  docker container run cowsay -l
+ 2043  docker container run cowsay -f dragon-and-cow "VQV Trybe!"
+ 2044  docker container run cowsay -f bunny  "VQV Trybe!"
