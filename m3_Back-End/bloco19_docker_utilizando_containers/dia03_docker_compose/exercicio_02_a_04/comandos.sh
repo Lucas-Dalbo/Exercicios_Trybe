d9f3381c034cb6d8cc4f4a6e56f8d4b9ed02f375
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-11 09:29:48 -0300

    Resolve 3 e 4 - Criar um serviço banco de dados mysql. - Cria volumes e networks.

--- m3_Back-End/bloco19_docker_utilizando_containers/dia03_docker_compose/exercicio_02_a_04/comandos.sh
+++ m3_Back-End/bloco19_docker_utilizando_containers/dia03_docker_compose/exercicio_02_a_04/comandos.sh
@@ -1,4 +1,4 @@
 docker-compose up -d # Para subir o Compose
-docker-compose down # Para descer o ompose
+docker-compose down # Para descer o Compose
 docker images # Para ver as images e identificar o ID
 docker rmi efcd4044e3a0 # Para apagar a imagem ghost
