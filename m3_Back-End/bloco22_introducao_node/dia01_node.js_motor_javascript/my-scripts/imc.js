74448823552cdfadeba1cac1c2ce6271d4264a07
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-26 18:46:51 -0300

    Feat: Implementa interação do usuário

--- m3_Back-End/bloco22_introducao_node/dia01_node.js_motor_javascript/my-scripts/imc.js
+++ m3_Back-End/bloco22_introducao_node/dia01_node.js_motor_javascript/my-scripts/imc.js
@@ -1,7 +1,12 @@
+const readline = require('readline-sync');
+
 const calculoIMC = (peso, altura) => (peso / Math.pow(altura, 2)).toFixed(2);
 
 function main () {
-  const resultado = calculoIMC(83, 1.7);
+  const altura = readline.questionFloat('Qual a sua altura em metros? ');
+  const peso = readline.questionFloat('Qual o seu peso em Kg? ');
+
+  const resultado = calculoIMC(peso, altura);
   console.log(resultado);
 }
 
