35df6dfd7fbab6c57cfbd6f3446c43bac7852f77
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-28 19:50:22 -0300

    feat: Implementa função writeFile

--- m3_Back-End/bloco22_introducao_node/dia03_mocha_chai_sino_testando/testes/writeFile.js
+++ m3_Back-End/bloco22_introducao_node/dia03_mocha_chai_sino_testando/testes/writeFile.js
@@ -1,15 +1,41 @@
 const { expect } = require("chai");
+const { writeFile } = require('../writeFile');
+const sinon = require('sinon');
+const fs = require('fs').promises;
 
 describe('A função "writeFile"', () => {
-  describe('Quando concluida', () => {
-    it('Retonar "ok"', () => {
-      const retorno = writeFile();
+  describe('Quando concluida com sucesso', () => {
+    before(() => {
+      sinon.stub(fs, 'writeFile').resolves();
+    });
+  
+    after(() => {
+      fs.writeFile.restore();
+    })
+
+    it('Retonar "ok"', async () => {
+      const retorno = await writeFile();
       expect(retorno).to.be.equal('ok');
     });
 
-    it('Retorna uma string', () => {
-      const retorno = writeFile();
+    it('Retorna uma string', async () => {
+      const retorno = await writeFile();
       expect(typeof retorno).to.be.a('string');
+    });
+  });
+
+  describe('Quando falhar', () => {
+    before(() => {
+      sinon.stub(fs, 'writeFile').rejects();
+    });
+    
+    after(() => {
+      fs.writeFile.restore();
+    })
+  
+    it('Retonar mensagem de erro', async () => {
+      const retorno = await writeFile();
+      expect(retorno).to.be.equal('Um erro ocorreu na criação do arquivo');
     });
   });
 });
