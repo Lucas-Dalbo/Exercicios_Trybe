c042f15ee67ad2fc133f78f20ebe60a1f2ec040c
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-28 19:16:47 -0300

    feat: Implementa função numberType

--- m3_Back-End/bloco22_introducao_node/dia03_mocha_chai_sino_testando/testes/numberType.js
+++ m3_Back-End/bloco22_introducao_node/dia03_mocha_chai_sino_testando/testes/numberType.js
@@ -1,16 +1,17 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
+const { numberType } = require('../numberType');
 
 describe('A função "numberType"', () => {
   describe('Quando o número é maior que 0', () => {
     it('retorna "positivo"', () => {
-      const resultado = numberType(-1);
+      const resultado = numberType(1);
       expect(resultado).to.be.equals('positivo');
     });
   });
 
   describe('Quando o número é menor que 0', () => {
     it('retorna "negativo"', () => {
-      const resultado = numberType(1);
+      const resultado = numberType(-1);
       expect(resultado).to.be.equals('negativo');
     });
   });
@@ -19,6 +20,13 @@ describe('A função "numberType"', () => {
     it('retorna "neutro"', () => {
       const resultado = numberType(0);
       expect(resultado).to.be.equals('neutro');
+    });
+  });
+
+  describe('Quando o valor passado não é um number', () => {
+    it('retorna mensagem de erro', () => {
+      const resultado = numberType('salsicha');
+      expect(resultado).to.be.equals('O valor deve ser um número');
     });
   });
 });
