2b6ad03e67d937bc748aab3a2395b0ddfdd95fc1
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-07-30 08:47:54 -0300

    feat: Atividade 1 A rota /ping retorna um json com pong

--- m3_Back-End/bloco22_introducao_node/dia04_api_http_express/index.js
+++ m3_Back-End/bloco22_introducao_node/dia04_api_http_express/index.js
@@ -3,3 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
