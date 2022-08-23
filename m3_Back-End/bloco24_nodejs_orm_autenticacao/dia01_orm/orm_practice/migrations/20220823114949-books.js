2bab2357679a2a2a246aa2918ce791992971aaaa
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-08-23 09:28:56 -0300

    feat: seed para Books + -updated e created agora tem valores default

--- m3_Back-End/bloco24_nodejs_orm_autenticacao/dia01_orm/orm_practice/migrations/20220823114949-books.js
+++ m3_Back-End/bloco24_nodejs_orm_autenticacao/dia01_orm/orm_practice/migrations/20220823114949-books.js
@@ -24,11 +24,13 @@ module.exports = {
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         field: 'created_at',
       },
       updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         field: 'updated_at',
       },
     });
