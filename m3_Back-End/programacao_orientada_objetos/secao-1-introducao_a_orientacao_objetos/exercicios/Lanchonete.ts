6c486ddc7e68ebfa0bd0a13a7d887a9d32372427
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-16 10:31:54 -0300

    feat: métodos p/ Pedidos - Adiciona método para calcular o total pedido

--- m3_Back-End/programacao_orientada_objetos/secao-1-introducao_a_orientacao_objetos/exercicios/Lanchonete.ts
+++ m3_Back-End/programacao_orientada_objetos/secao-1-introducao_a_orientacao_objetos/exercicios/Lanchonete.ts
@@ -14,6 +14,10 @@ class Item {
     this._nome = nome;
     this._preco = preco;
   }
+
+  get preco() {
+    return this._preco;
+  }
 };
 
 class Pedido {
@@ -28,6 +32,18 @@ class Pedido {
     this._pagamento = pagamento;
     this._desconto = desconto;
   }
+
+  public totalPedido(): string {
+    const total = this._consumo.reduce((acc, crr) => acc + crr.preco, 0);
+
+    return total.toFixed(2);
+  }
+
+  public totalcomDesc(): string {
+    const soma = this._consumo.reduce((acc, crr) => acc + crr.preco, 0);
+    const total = soma - soma*(this._desconto || 0);
+    return total.toFixed(2);
+  }
 };
 
 const jorge = new Cliente('Jorge');
@@ -40,3 +56,5 @@ const choco = new Item('Chocolate', 3);
 const pedido01 = new Pedido(jorge, [xSalada, suco, choco], 'débito', 0.1);
 
 console.log(pedido01);
+console.log(pedido01.totalPedido());
+console.log(pedido01.totalcomDesc());
