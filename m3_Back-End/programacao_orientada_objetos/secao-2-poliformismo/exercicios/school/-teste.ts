148ce4b010fce2e06ce3d0014389148a73500806
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-20 09:44:14 -0300

    feat: arquivo -teste.ts - Criado apenas para testar a aplicação, divirtasse!

---
+++ m3_Back-End/programacao_orientada_objetos/secao-2-poliformismo/exercicios/school/-teste.ts
@@ -0,0 +1,38 @@
+import EvaluationResult from "./EvaluationResult";
+import Exam from "./Exam";
+import Student from "./StudentPerson";
+import Subject from "./Subject";
+import Teacher from "./Teacher";
+import Work from "./Work";
+
+const mat = new Subject('Matemática');
+const geo = new Subject('Geografia');
+
+const mTeacher = new Teacher('Tony', '03/08/1980', mat, 3000);
+const gTeacher = new Teacher('Cintia', '12/05/1989', geo, 3200);
+
+console.log(mTeacher);
+console.log(gTeacher);
+
+const mExam = new Exam(25, mTeacher);
+const mWork = new Work(50, mTeacher);
+
+const gExam = new Exam(25, gTeacher);
+const gWork = new Work(50, gTeacher);
+
+const student1 = new Student('Mario', '07/07/2007');
+const result11 = new EvaluationResult(mExam, 20);
+const result12 = new EvaluationResult(mWork, 20);
+
+student1.setEvaluationResult(result11);
+student1.setEvaluationResult(result12);
+
+const student2 = new Student('Luigi', '07/07/2007');
+const result21 = new EvaluationResult(gExam, 20);
+const result22 = new EvaluationResult(gWork, 20);
+
+student2.setEvaluationResult(result21);
+student2.setEvaluationResult(result22);
+
+console.log(student1);
+console.log(student2);
