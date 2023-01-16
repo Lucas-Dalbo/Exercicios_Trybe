8e145a0b436cc5409e891434784e96f82f52c420
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-16 08:43:33 -0300

    refac: Remove redundância de código

--- m4_Ciências-da-Computação/secao-3-redes_e_raspagem_de_dados/dia-02-raspagem_de_dados/exercicio02.py
+++ m4_Ciências-da-Computação/secao-3-redes_e_raspagem_de_dados/dia-02-raspagem_de_dados/exercicio02.py
@@ -5,4 +5,4 @@ response = requests.get("https://api.github.com/users")
 data_list = json.loads(response.text)
 
 for user in data_list:
-    print(user["login"], user["url"], sep="")
+    print(user["login"], user["url"])
