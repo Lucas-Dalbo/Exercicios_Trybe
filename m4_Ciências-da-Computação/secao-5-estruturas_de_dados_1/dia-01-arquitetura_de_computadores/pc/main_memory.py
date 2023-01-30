b9c277538b78bb0e5ee4d6986de115f1d1fd6cbe
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-30 18:30:22 -0300

    feat: main_memory.py Classe MainMemory que simula uma memória primaria

---
+++ m4_Ciências-da-Computação/secao-5-estruturas_de_dados_1/dia-01-arquitetura_de_computadores/pc/main_memory.py
@@ -0,0 +1,20 @@
+class MainMemory:
+    def __init__(self):
+      self.clean()
+      self.loaded_memory = []
+
+
+    def load(self, value):
+        self.loaded_memory.append(value)
+
+
+    def get(self, index):
+        try:
+            value = self.loaded_memory[index]
+            return value
+        except IndexError:
+            return 0
+
+
+    def clean(self):
+        self.loaded_memory = []
