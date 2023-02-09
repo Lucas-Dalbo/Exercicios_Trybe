a97148bb72d1859ca20b61dad9a10c8a46d515ed
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-02-09 09:58:41 -0300

    fix: remove testes manuais

--- m4_Ciências-da-Computação/secao-6-estrutura_de_dados_2/dia-01-hashmaps_e_dicts/exercicio02.py
+++ m4_Ciências-da-Computação/secao-6-estrutura_de_dados_2/dia-01-hashmaps_e_dicts/exercicio02.py
@@ -8,15 +8,6 @@ def score_of(staff, person):
     
     return score
 
-staff = {
-    1: [2, 3],
-    2: [4],
-    3: [],
-    4: [],
-}
-
-print(score_of(staff, 1))
-
 # Analise de compelxidade:
 # A função itera por todos os membros de uma lista correspondente a chave informada.
 # Logo, a complexidade é O(n1 + n2 + ... + nx), cada n o tamanho da lista que sera iterada.
